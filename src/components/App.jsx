620bac7e98bac687d5962402e887ae820ea2f617
Ben-cod <123839625+Ben-cod@users.noreply.github.com>
2023-08-29 14:14:13 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,23 +8,19 @@ import { nanoid } from 'nanoid';
 import React from 'react';
 import { Filter } from './Filter/Filter';
 
+const phoneContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', phone: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', phone: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', phone: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', phone: '227-91-26' },
+];
+
 const getContacts = () => {
   const contacts = localStorage.getItem('contacts');
-  const parsedContacts = JSON.parse(contacts);
-
-  if (parsedContacts !== null) {
-    return JSON.parse(contacts);
-  }
+  return JSON.parse(contacts) || phoneContacts;
 };
 
 export const App = () => {
-  const phoneContacts = [
-    { id: 'id-1', name: 'Rosie Simpson', phone: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', phone: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', phone: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', phone: '227-91-26' },
-  ];
-
   const [contacts, setContacts] = useState(getContacts || phoneContacts);
   const [filter, setFilter] = useState('');
 
